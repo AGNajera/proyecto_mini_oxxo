0eca866822243f5a758a6cec3d7e31d1840d6523
AGNajera <aemiliano9@gmail.com>
2025-08-03 18:13:41 -0600

    Actualizar cantidad

--- actualizacion/act_cant.py
+++ actualizacion/act_cant.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import db.conexion as Conn
 import os
 
@@ -7,14 +7,14 @@ def act_cantidad():
     while True:
         os.system("cls" if os.name == "nt" else "clear")
         print("ACTUALIZAR CANTIDAD DE UN PRODUCTO")
-        conf = input("¿Desea continuar con el cambio de precio de un producto? (s/n): ")
+        conf = input("¿Desea continuar con la actualización de la cantidad de un producto? (s/n): ")
         if conf.lower() == 'n':
-            print("Cancelando actualización de precio de un producto.")
+            print("Cancelando actualización de cantidad de un producto.")
             input("Presione cualquier tecla para continuar...")
             return
         elif conf.lower() == 's':
             encontrado = None
-            id_producto = int(input("ID del producto a cambiar su descripción: "))
+            id_producto = int(input("ID del producto a actualizar su cantidad: "))
             if id_producto <= 0:
                 print("El ID del producto debe mayor a 0.")
                 input("Presione cualquier tecla para continuar...")
@@ -23,7 +23,7 @@ def act_cantidad():
             print("Producto seleccionado de acuerdo al ID tecleado")
             for producto in cursor:
                 encontrado = True
-                print(f"\nID: {producto[0]}, Descripción: {producto[1]}, Precio: ${producto[2]}, Cantidad: {producto[3]}")
+                print(f"\nID: {producto[0]}, Descripción: {producto[1]}, Precio: ${producto[2]}, Cantidad: {producto[3]}, Fecha: {producto[4]}")
                 conf = ''
                 conf = input("\n¿Desea actualizar la cantidad de este producto? (s/n): ")
                 if conf.lower() == 'n':
@@ -32,26 +32,23 @@ def act_cantidad():
                     return act_cantidad()
                 elif conf.lower() == 's':
                     cant_nuevo = float(input("Nueva cantidad del producto: "))
-                    fecha_ingreso = datetime.strptime(input("Fecha de ingreso del producto (Año-Mes-Día): "), "%Y-%m-%d")
+                    fecha_ingreso = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    print(f"Fecha del actualización: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}")
                     cursor = Conn.conexion.execute(f"SELECT cantidad FROM productos WHERE cantidad = {cant_nuevo}")
-                    if fecha_ingreso > datetime.now():
-                        print("La fecha de ingreso no puede ser futura.")
-                        input("Presione cualquier tecla para continuar...")
-                        return act_cantidad()
                     if cant_nuevo == "" or cant_nuevo == " ":
                         print("La cantidad del producto no puede estar vacia o contener un espacio en blanco")
                         input("Presione cualquier tecla para continuar...")
                         return act_cantidad()
                     if cant_nuevo <=0:
-                        print("El precio debe de ser mayor a 0")
+                        print("La cantidad debe de ser mayor a 0")
                         input("Presione cualquier tecla para continuar...")
                         return act_cantidad()
                     else:
                         cursor = Conn.conexion.execute(
-                            "UPDATE productos SET cantidad += ?, fecha = ? WHERE id = ?",
+                            "UPDATE productos SET cantidad = cantidad + ?, fecha = ? WHERE id = ?",
                             (cant_nuevo, fecha_ingreso, id_producto)
                         )
-                    print("Descripción de producto actualizada.")
+                    print("Cantidad de producto actualizada.")
                     input("Presione cualquier tecla para continuar...")
                     Conn.conexion.commit()
                     return
